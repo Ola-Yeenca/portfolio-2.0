8a601bc89afd2b6450bbcd8c21851cdbbd333022
Timothy Olaonipekun Omoniyi <olayinkatimothy102@gmail.com>
2022-11-16 16:00:00 +0100

    building porfolio with nextjs

---
+++ components/Header.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import { SocialIcon } from 'react-social-icons';
+import { motion } from "framer-motion";
+
+type Props = {}
+
+export default function header({}: Props) {
+  return (
+    <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'>
+      <motion.div
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5
+        }}
+
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1
+        }}
+
+        transition={{
+          duration: 1.5,
+          type: 'spring',
+        }}
+
+      className='flex flex-row items-center'>
+        {/* Social Icons */}
+        <SocialIcon
+        url="https://twitter.com/Ola_Yeenca"
+        fgColor='grey'
+        bgColor='transparent'
+        />
+        <SocialIcon
+        url="https://twitter.com/Ola_Yeenca"
+        fgColor='grey'
+        bgColor='transparent'
+        />
+        <SocialIcon
+        url="https://twitter.com/Ola_Yeenca"
+        fgColor='grey'
+        bgColor='transparent'
+        />
+        <SocialIcon
+        url="https://twitter.com/Ola_Yeenca"
+        fgColor='grey'
+        bgColor='transparent'
+        />
+      </motion.div>
+
+      <motion.div
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5
+        }}
+
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1
+        }}
+
+        transition={{
+          duration: 1.5,
+          type: 'spring', 
+        }}
+
+      className='flex flex-row items-center text-grey-300 cursor-pointer'>
+        <SocialIcon
+        className='cursor-pointer'
+        network='email'
+        fgColor='grey'
+        bgColor='transparent'
+        />
+        <p className='
+        uppercase
+        hidden
+        md:inline-flex
+        text-sm
+        text-grey-200'>
+          Get In Touch
+        </p>
+      </motion.div>
+    </header>
+  );
+}
---
+++ components/Hero.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Cursor, useTypewriter } from 'react-simple-typewriter';
+import BackgroundCircles from './BackgroundCircles';
+import { motion } from "framer-motion";
+import Link from 'next/link';
+
+
+
+type Props = {}
+
+export default function Hero({}: Props) {
+  const [text, count] = useTypewriter({
+    words: [
+    'Hello, name is Ola Yinka ',
+    'Developer',
+    'Designer',
+    'Freelancer'
+  ],
+    loop: true,
+    delaySpeed: 2000,
+  })
+  return (
+    <motion.div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
+      {/* <BackgroundCircles /> */}
+      <img
+      className='
+      relative
+      rounded-full
+      h-52 w-42
+      mx-auto
+      object-cover'
+        src='/Image/profile-picture.png' alt='Ola-Yinka'/>
+
+        <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>
+          Software Engineer
+        </h2>
+      <h1 className='text-5xl lg:text-6xl font-semibold px-10'>
+        <span className='mr-3'>{text}</span>
+        <Cursor cursorColor='black'/>
+      </h1>
+
+      <div className='pt-5'>
+        <Link href='#about'>
+          <button className='heroButton'>About</button>
+        </Link>
+
+        <Link href='#experience'>
+          <button className='heroButton'>Experience</button>
+        </Link>
+
+        <Link href='#skills'>
+          <button className='heroButton'>Skills</button>
+        </Link>
+
+        <Link href='#projects'>
+          <button className='heroButton'>Projects</button>
+        </Link>
+      </div>
+    </motion.div>
+  )
+}
